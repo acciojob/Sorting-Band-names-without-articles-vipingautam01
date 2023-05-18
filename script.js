969d21f834c7ce4ce6ca2fad2643bf4182ca55b5
Vipin Gautam <130040245+vipingautam01@users.noreply.github.com>
2023-05-18 15:49:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
 //your code here
 
+let bandNames = ['The Rolling Stones', 'Led Zeppelin', 'The Beatles', 'Aerosmith'];
+
+// Function to remove articles from the band name
+function removeArticles(name) {
+  return name.replace(/^(a |an |the )/i, '');
+}
+
+// Sort the band names in lexicographic order (excluding articles)
+bandNames.sort((a, b) => {
+  const nameA = removeArticles(a.toUpperCase());
+  const nameB = removeArticles(b.toUpperCase());
+  if (nameA < nameB) {
+    return -1;
+  } else if (nameA > nameB) {
+    return 1;
+  }
+  return 0;
+});
+
+// Create a UL element with sorted band names as LI elements
+const ulElement = document.createElement('ul');
+ulElement.id = 'band';
+
+bandNames.forEach(name => {
+  const liElement = document.createElement('li');
+  liElement.textContent = name;
+  ulElement.appendChild(liElement);
+});
+
+// Add the UL element to the document body
+document.body.appendChild(ulElement);
